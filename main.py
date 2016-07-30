1d0c60105246ef1a0c1658f3606ec6685ad1724a
buchefa <fwulfes@gmx.net>
2016-07-30 12:16:36 +0200

    Logic

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ curState = 0            # 0: deactivated; 1: activated
 GPIO.setup(11, GPIO.input)  # switch
 
 GPIO.setup(10, GPIO.output) # LED
+
+while 1:
+    if GPIO.input(11) ==GPIO.HIGH:
+        GPIO.output(10,GPIO.HIGH)
+    else:
+        GPIO.output(10,GPIO.LOW)
+    
